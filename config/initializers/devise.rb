6cce1484b39880ed9cc9970042b2392c4c9f22a0
Felipe Vale <85165362+FelipePaivaVale@users.noreply.github.com>
2025-05-05 19:54:03 -0300

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,7 +20,7 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
   config.jwt do |jwt|
-    jwt.secret = ENV['JWT_SECRET'] || Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.secret = ENV.fetch('JWT_SECRET') { SecureRandom.hex(32) }
     jwt.dispatch_requests = [['POST', %r{^/login$}]]
     jwt.revocation_requests = [['DELETE', %r{^/logout$}]]
     jwt.expiration_time = 1.day.to_i
