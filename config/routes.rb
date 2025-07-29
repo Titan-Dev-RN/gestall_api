7aa3a06f945ab48791b76f4756ccbde8617cdd98
felipepaivavale <felipepaivavale@gmail.com>
2025-07-29 18:55:17 -0300

    hotfix de merda q fiz

--- config/routes.rb
+++ config/routes.rb
@@ -35,9 +35,8 @@ Rails.application.routes.draw do
         end
         collection do
           get 'baixo_estoque'
-          post 'criar_categoria'
           get 'categorias'
-          get 'por_categoria/:categoria'
+          get 'por_categoria/:categoria', action: :por_categoria
         end
       end
       resources :clientes, only: [:index, :show, :create, :update, :destroy] do
