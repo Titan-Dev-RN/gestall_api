234d0feb5949c941dde5556dff5eefc03c4eb60e
felipepaivavale <felipepaivavale@gmail.com>
2025-07-30 08:39:14 -0300

    fix permissoes

--- app/models/usuario_permissao.rb
+++
@@ -1,8 +0,0 @@
-class UsuarioPermissao < ApplicationRecord
-  belongs_to :usuario
-  belongs_to :permissao
-
-  validates :usuario_id, uniqueness: { scope: :permissao_id }
-
-  audited associated_with: :usuario
-end
