3ea0a1694afb478570b96c3c4594c8dc800fba22
felipepaivavale <felipepaivavale@gmail.com>
2025-09-16 13:50:37 -0300

    atualização de lotes

--- app/controllers/api/v1/lotes_controller.rb
+++ app/controllers/api/v1/lotes_controller.rb
@@ -1,2 +1,51 @@
-class LotesController < ApplicationController
+class Api::V1::LotesController < ApplicationController
+    before_action :set_lote, only: [:show, :update, :destroy]
+
+    def index
+        lotes = Lote.where(informacao_loja_token: @current_user.token_integracao_loja)
+        render json: lotes
+    end
+
+    def show
+        render json: @lote
+    end
+
+    def create
+        lote = Lote.new(lote_params)
+        lote.informacao_loja_token = @current_user.token_integracao_loja
+
+        if lote.save
+            render json: lote, status: :created
+        else
+            render json: { errors: lote.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    def update
+        if @lote.update(lote_params)
+            render json: @lote
+        else
+            render json: { errors: @lote.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @lote.destroy
+            render json: { message: 'Lote deleted successfully' }
+        end
+    end
+
+    private
+
+    def set_lote
+        @lote = Lote.find_by(id: params[:id], informacao_loja_token: @current_user.token_integracao_loja)
+        if @lote.nil?
+            render json: { error: 'Lote não encontrado' }, status: :not_found
+        end
+    end
+
+    def lote_params
+        params.require(:lote).permit(:codigo, :data_validade, :quantidade, :produto_id)
+    end
+
 end
